4b9c183042909d955cd2f1ce2c00c677a134b921
Colin Millar <colin.millar@ices.dk>
2024-03-07 11:30:57 +0100

    Initial commit

---
+++ Metiers/Scripts/Functions/loadAreaList.R
@@ -0,0 +1,12 @@
+loadAreaList <- function(url){
+  message("Loading area list ...")
+  x <- data.table(read.csv(url, sep = ",", stringsAsFactors = F))
+  setnames(x, old = c("Code","AreaCode"), new = c("RCG","area"))
+  x <- x[,map(.SD,trimws)]
+  # area 21.0.A is present twice for NA and NSEA. In the next step we merge by area so we must get rid of duplicated areas. Question is how to handle 21.0.A.
+  if(any(duplicated(x$area))){
+    x<-x[!duplicated(x$area)]
+    print("Warning! Duplicated area codes were found and were removed.")
+  }
+  return(x[,.(area,RCG)])
+}
