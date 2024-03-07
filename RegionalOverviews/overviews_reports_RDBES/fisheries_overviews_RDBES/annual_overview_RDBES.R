d66eb98c98b535a35ca3f6f5a1e48d5616d8990b
Colin Millar <colin.millar@ices.dk>
2024-03-07 11:31:57 +0100

    Initialize marta_choropleth_map

---
+++ RegionalOverviews/overviews_reports_RDBES/fisheries_overviews_RDBES/annual_overview_RDBES.R
@@ -0,0 +1,22 @@
+# annual_overview_RDBES_template.Rmd - contains main structure of the overview, but to create a report, please use annual_overview_RDBES.R - 
+#   - it sources the .rmd file and additionally in annual_overview_RDBES.R you can define parameters to be used in the report
+
+params <- list(
+  year = 2021,
+  region = 'BA', #BA, NA, NSEA
+  logo_path = file.path("../../overviews_shiny/www/logo RCG BALTIC.PNG"), # move it to the rmd part <----------------- to do 
+  data_dir = '../../data_RDBES/002_prepared/20240129/RCG_BA',
+  CLfileName = 'RDBES_RCG_BA_CL_2021_2021_prepared_20240129',
+  CEfileName = 'RDBES_RCG_BA_CE_2021_2021_prepared_20240129',
+  RDBES_download_date = '01/01/2000'
+  
+)
+rmdReport <- file.path("RegionalOverviews/overviews_reports_RDBES/fisheries_overviews_RDBES/annual_overview_RDBES_template.Rmd")
+
+rmarkdown::render(
+  rmdReport,
+  params = params,
+  output_file = paste0('results/AnnualOverview_', params$year ,'_', params$region, '.html'), # reports saved into results folder
+  envir = new.env(parent = globalenv()),
+  encoding = 'UTF-8'
+)
---
+++ RegionalOverviews/overviews_reports_RDBES/fisheries_overviews_RDBES/scripts/loadData.R
@@ -0,0 +1,22 @@
+# load CL data
+load(
+  paste(params$data_dir, '/', params$CLfileName,'.Rdata', sep = "")
+)
+cl = cl_rcg # shorter name 
+
+#load CE data
+
+load(
+  paste(params$data_dir, '/' ,params$CEfileName,'.Rdata', sep = "")
+)
+
+ce = ce_rcg # shorter name 
+
+# put some necessary data prep part below
+
+######################
+# FILTER the data out
+######################
+
+cl <- cl[CLyear %in% params$year]
+ce <- ce[CEyear %in% params$year]
