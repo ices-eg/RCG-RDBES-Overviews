67b21b15af0bca50255bf8b5d72f61fe70632678
Kasia-MIR <60775640+Kasia-MIR@users.noreply.github.com>
2025-03-10 12:01:07 +0100

    update summary table

---
+++ RegionalOverviews/overviews_reports_RDBES/sampling_overviews_RDBES/scripts/SAspecies.R
@@ -0,0 +1,40 @@
+cs |>
+  select(DEhierarchy, SDctry, SAspeCode) |>
+  distinct() |>
+  group_by(SDctry, DEhierarchy) |>
+  count() |>
+  arrange(DEhierarchy) |>
+  pivot_wider(names_from = DEhierarchy, values_from = n) |>
+  rename(`Country/Hierarchy` = SDctry) -> SAsummary
+
+cs |>
+  select(SDctry, SAspeCode) |>
+  distinct() |>
+  group_by(SDctry) |>
+  count() |>
+  rename(`Country/Hierarchy` = SDctry,
+         Total = n) -> summarizedCol
+
+SAsummary <- SAsummary |>
+  left_join(summarizedCol, by = "Country/Hierarchy")
+
+summarizedRow <- cs |>
+  select(DEhierarchy, SAspeCode) |>
+  distinct() |>
+  group_by(DEhierarchy) |>
+  count() |>
+  arrange(DEhierarchy) |>
+  pivot_wider(names_from = DEhierarchy, values_from = n)|>
+  mutate(`Country/Hierarchy` = "Total")
+
+summarizedRowTotal <- cs |>
+  select(SAspeCode) |>
+  distinct() |>
+  count() |>
+  rename(Total = n)
+
+cbind(summarizedRow,summarizedRowTotal) -> summarizedRow
+SAsummary <- bind_rows(SAsummary, summarizedRow)
+
+SAsummary <- SAsummary |>
+  mutate(across(everything(), ~ replace_na(as.character(.), "")))
