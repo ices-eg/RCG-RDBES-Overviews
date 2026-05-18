545a2a7e72eb1575fe69747cc23cc28f7bdff3b0
Kasia-MIR <kkrakowka@mir.gdynia.pl>
2026-05-18 15:34:45 +0200

    the latest data version

--- RegionalOverviews/overviews_reports_RDBES/case_studies_overviews_RDBES/scripts/parametersDefinitionMultiannual.R
+++ RegionalOverviews/overviews_reports_RDBES/case_studies_overviews_RDBES/scripts/parametersDefinitionMultiannual.R
@@ -28,7 +28,7 @@ params <- list(
   region = regionSelected, 
   logo_path = ifelse(regionSelected=='BA',file.path("../../data/logo/logo RCG BALTIC.PNG"),file.path("../../data/logo/logo RCG NA NS_EA.PNG")),
   data_dir_CS = paste0(getwd(), '/RegionalOverviews/data_RDBES/002_prepared/', dataprepDate, '/RCG_', regionSelected),
-  CSfileName = paste0('RDBES_RCG_', regionSelected, '_CS_',yearSelected-2,'_',yearSelected,'_prepared_', dataprepDate),
+  CSfileName = paste0('RDBES_RCG_', regionSelected, '_CS_',yearSelected-4,'_',yearSelected,'_prepared_', dataprepDate),
   data_dir_CL = paste0(getwd(), '/RegionalOverviews/data_RDBES/002_prepared/', dataprepDateCL, '/RCG_', regionSelected),
   CLfileName = paste0('RDBES_RCG_', regionSelected, '_CL_',yearSelected-4,'_',yearSelected,'_prepared_', dataprepDateCL),
   data_dir_CE = paste0(getwd(), '/RegionalOverviews/data_RDBES/002_prepared/', dataprepDateCE, '/RCG_', regionSelected),
